f39321d9bda2027e24c7537ed48644ae226d98d5
15138730370 <1037483576@qq.com>
2017-04-04 10:50:25 +0800

    错误页

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DownloadBoxComponent } from './download-box/download-box.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports:[
-    DownloadBoxComponent
+    DownloadBoxComponent,
+    ErrorComponent
   ],
   declarations: [
-    DownloadBoxComponent
+    DownloadBoxComponent,
+    ErrorComponent
   ]
 })
 export class BaseModule { }
